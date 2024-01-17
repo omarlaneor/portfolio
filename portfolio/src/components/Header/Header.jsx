e45999ac71c42747ccd60232ed75712850b4d15c
omarlaneor <annielaneor@gmail.com>
2024-01-17 12:08:45 -0500

    Actualización

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <h1>
-        Hola, soy <span className="highlight">Omar Osorio</span>
-      </h1>
+    <header className="header">
+      <div className="header-content">
+        <aside>
+          <h1>
+            Hola, soy <span className="highlight">Omar Osorio</span>
+          </h1>
+          <p>
+            Establecido en Colombia, soy un estudiante de Web-Full Stack
+            Development y Análisis y Desarrollo de Software, con estudios en la
+            Universidad de Brigham Young Idaho en "Applied Technology BS".
+          </p>
+        </aside>
+        <aside>
+          <img src="../../../public/In The Office.jpg" alt="Omar Osorio" />
+        </aside>
+      </div>
     </header>
   );
 };
--- portfolio/src/components/Portfolio/Portfolio.jsx
+++ portfolio/src/components/Portfolio/Portfolio.jsx
@@ -3,12 +3,13 @@ import React from "react";
 const Portfolio = () => {
   return (
     <section className="portfolio">
-      <img src="../../../public/In The Office.jpg" alt="Omar Osorio" />
-      <p>
-        Establecido en Colombia, soy un estudiante de Web-Full Stack Development
-        y Análisis y Desarrollo de Software, con estudios en la Universidad de
-        Brigham Young Idaho en "Applied Technology BS".
-      </p>
+      <div className="portfolio-content">
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        <p>PHP</p>
+        <p>React</p>
+      </div>
     </section>
   );
 };
